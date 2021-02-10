17358401d7811162f09302b8f81255c368bcb599
Alemasov Sergey Aleksandrovich <s.alemasov@tinkoff.ru>
2021-02-10 19:16:55 +0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { SomeClass } from './some-class';
+
+const someClassInstance: SomeClass = new SomeClass();
+
+someClassInstance.printX();
+someClassInstance.calculateSum(10, 11);
---
+++ src/some-class.ts
@@ -0,0 +1,11 @@
+export class SomeClass {
+    private x: number = 10;
+
+    public printX(): void {
+        console.log(this.x);
+    }
+
+    public calculateSum(a: number, b: number): number {
+        return a + b;
+    }
+}
